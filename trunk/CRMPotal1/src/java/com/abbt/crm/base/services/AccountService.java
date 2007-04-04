fd229305171ae088bad643fe65324f5bd672abb6
osmancis <osmancis@1dea1c45-a02c-0410-9394-a5799b269e0d>
2007-04-04 04:15:38 +0000

    First Creation AccountService Interface

---
+++ trunk/CRMPotal1/src/java/com/abbt/crm/base/services/AccountService.java
@@ -0,0 +1,26 @@
+/*
+ * AccountService.java
+ *
+ * Created on April 3, 2007, 8:32 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbt.crm.base.services;
+
+import com.abbt.crm.base.entity.Account;
+import java.util.List;
+import com.abbt.crm.base.entity.Account;
+
+/**
+ *
+ * @author Osman
+ */
+public interface AccountService {
+    /*
+     * declaring the interface for recent employee accounts
+     */
+    List<Account> findRecentEmployeeAccounts();
+    
+}
