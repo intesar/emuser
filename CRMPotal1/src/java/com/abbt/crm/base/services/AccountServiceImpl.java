34f9bf481c95f0e866a907300123004337801807
osmancis <osmancis@1dea1c45-a02c-0410-9394-a5799b269e0d>
2007-04-04 04:16:39 +0000

    First working creation of AccountServiceImp.java class

---
+++ CRMPotal1/src/java/com/abbt/crm/base/services/AccountServiceImpl.java
@@ -0,0 +1,43 @@
+/*
+ * AccountServiceImpl.java
+ *
+ * Created on April 3, 2007, 8:32 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbt.crm.base.services;
+
+import com.abbt.crm.base.dao.GenericDAO;
+import com.abbt.crm.base.dao.GenericDAOImpl;
+import com.abbt.crm.base.entity.Account;
+import java.util.List;
+ 
+/**
+ *
+ * @author Osman
+ */
+public class AccountServiceImpl implements AccountService {
+    
+    private GenericDAO dao = new GenericDAOImpl();
+    /** Creates a new instance of AccountServiceImpl */
+    public AccountServiceImpl() {
+    }
+    
+    public List<Account> findRecentEmployeeAccounts() {
+        
+        /*
+         * Passing the query to the DAO.
+         */
+        
+        String jpaQuery = " select account from Account account order by account.lastModifiedDate";
+        return dao.find(jpaQuery);
+    
+    }
+    
+    public void setGenericDAO( GenericDAO dao) {
+        this.dao = dao;
+    }
+    
+}
