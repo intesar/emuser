460fd37ea5c8dc68cea02fc1931dabea0c0e4d11
osmancis <osmancis@1dea1c45-a02c-0410-9394-a5799b269e0d>
2007-04-05 04:45:55 +0000

    first working interface Oppertunity

---
+++ trunk/CRMPotal1/src/java/com/abbt/crm/base/services/Oppertunity.java
@@ -0,0 +1,21 @@
+/*
+ * Oppertunity.java
+ *
+ * Created on April 4, 2007, 10:35 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbt.crm.base.services;
+
+import java.util.List;
+
+/**
+ *
+ * @author Osman
+ */
+public interface Oppertunity {
+    
+    List<Oppertunity> findRecentOppertunity();
+}
