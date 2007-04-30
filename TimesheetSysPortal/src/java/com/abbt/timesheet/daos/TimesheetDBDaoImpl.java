45c9d3e40c8657077b31acefde0f89074313e623
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2007-04-30 03:02:52 +0000

    dao is created

---
+++ TimesheetSysPortal/src/java/com/abbt/timesheet/daos/TimesheetDBDaoImpl.java
@@ -0,0 +1,130 @@
+/*
+ * TimesheetDBDaoImpl.java
+ *
+ * Created on April 29, 2007, 9:45 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbt.timesheet.daos;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author shannan
+ */
+public class TimesheetDBDaoImpl {
+    
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("TimesheetSysPortalPU");
+    
+    
+    
+    /** Creates a new instance of TimesheetDBDaoImpl */
+    public TimesheetDBDaoImpl() {
+    }
+    
+    public void save(Object object) {
+        EntityManager em = null;
+        try {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,"exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        
+    }
+    
+    public Object update(Object object) {
+        EntityManager em = null;
+        try {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            object = em.merge(object);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,"exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        return object;
+    }
+    public void delete(Object object) {
+        EntityManager em = null;
+        try {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.remove(object);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,"exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        
+    }
+    
+    public Object findByPrimaryKey(Class clazz, Object key ) {
+        EntityManager em = null;
+        Object obj = null;
+        try {
+            em = emf.createEntityManager();
+            obj = em.find(clazz, obj);
+        } catch(Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,"exception caught", e);
+        } finally {
+            em.close();
+        }
+        return obj;
+    }
+    
+    public Object findSingleResultByNamedQuery( String namedQuery, Object... obj ) {
+        Object resultObj = null;
+        EntityManager em = null;
+        try {
+            em = emf.createEntityManager();
+            Query q = em.createNamedQuery(namedQuery);
+            for ( int i = 0; i < obj.length; i++) {
+                q.setParameter(i, obj[i]);
+            }
+            resultObj = q.getSingleResult();
+        } catch(Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,"exception caught", e);            
+        } finally {
+            em.close();
+        }
+        return resultObj;
+    }
+    
+    public List findResultListByNamedQuery( String namedQuery, Object... obj)  {
+        List list = null;
+        EntityManager em = null;
+        try {
+            em = emf.createEntityManager();
+            Query q = em.createNamedQuery(namedQuery);
+            for ( int i = 0; i < obj.length; i++) {
+                q.setParameter(i, obj[i]);
+            }
+            list = q.getResultList();
+        } catch(Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,"exception caught", e);
+        } finally {
+            em.close();
+        }
+        return list;
+    }
+}
