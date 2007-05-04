6bd63244f2bd2c8715ae024db6097bf9be7e48f7
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2007-05-04 04:13:32 +0000

    TimesheetService added

--- TimesheetSysPortal/src/java/com/abbt/timesheet/portlets/timesheetCreator/TimesheetCreator.java
+++ TimesheetSysPortal/src/java/com/abbt/timesheet/portlets/timesheetCreator/TimesheetCreator.java
@@ -2,7 +2,11 @@ package com.abbt.timesheet.portlets.timesheetCreator;
 import com.abbt.timesheet.entities.Timesheet;
 import com.abbt.timesheet.entities.TimesheetDetail;
 import com.abbt.timesheet.entities.User;
+import com.abbt.timesheet.exceptions.EntityExistsException;
+import com.abbt.timesheet.services.ServiceFactory;
+import com.abbt.timesheet.services.TimesheetService;
 import java.text.ParseException;
+import java.util.Collection;
 import java.util.Date;
 import javax.portlet.GenericPortlet;
 import javax.portlet.ActionRequest;
@@ -12,6 +16,8 @@ import javax.portlet.RenderResponse;
 import javax.portlet.PortletException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import javax.portlet.PortletRequestDispatcher;
 
 public class TimesheetCreator extends GenericPortlet {
@@ -23,12 +29,14 @@ public class TimesheetCreator extends GenericPortlet {
         // Add your date format here.
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
         java.sql.Date sqlDate = null;
+        Calendar timesheetDate = Calendar.getInstance();
         try {
             Date utilDate = new Date( sdf.parse(startDate).getTime() );
             DateValidator.validate( utilDate );
+            timesheetDate.setTime(utilDate);
             sqlDate = new java.sql.Date(utilDate.getTime());
         } catch (ParseException ex) {
-            response.setRenderParameter("error", " Invalid Date Please Enter date starts with 1 or 16");
+            response.setRenderParameter("error", " Invalid Date, Day should be 1 or 16");
             ex.printStackTrace();
         }
         
@@ -36,37 +44,74 @@ public class TimesheetCreator extends GenericPortlet {
         if ( sqlDate instanceof java.sql.Date ) {
             
             Timesheet timesheet = new Timesheet();
-            //timesheet.setClientCompany(clientCompany);
+            //UserCompany userCompany = new UserCompany();
+            //userCompany.setCompanyName(clientCompany);
+            //timesheet.setClientCompany(userCompany);
             timesheet.setCreatedBy(request.getRemoteUser());
-            java.sql.Date todaysDate = new java.sql.Date ( new Date().getTime() );
+            java.sql.Date todaysDate = new java.sql.Date( new Date().getTime() );
             timesheet.setCreatedDate(todaysDate);
             timesheet.setDaysFor(15);
             timesheet.setLastUpdatedBy(request.getRemoteUser());
             timesheet.setLastUpdatedDate(todaysDate);
             timesheet.setTimesheetDate(sqlDate);
             User user = new User();
-            //timesheet.setUserEmail(user);
+            user.setEmail(request.getRemoteUser());
+            timesheet.setUserEmail(user);
+            Collection<TimesheetDetail> timesheetDetailCollection = new ArrayList<TimesheetDetail>();
             
             // creating timesheet details objects
+            int startIndex = 1;
+            int max = 15;
+            if ( timesheetDate.get(Calendar.DAY_OF_MONTH) == 16 ) {
+                startIndex = 16;
+                max = timesheetDate.getMaximum(Calendar.DAY_OF_MONTH);
+            }
+            for ( int i = startIndex; i <= max; i++ ) {
+                TimesheetDetail timesheetDetail = new TimesheetDetail();
+                timesheetDetail.setComments("");
+                timesheetDetail.setCreatedBy(request.getRemoteUser());
+                timesheetDetail.setCreatedDate(todaysDate);
+                timesheetDetail.setDay("");
+                timesheetDetail.setEnabled(true);
+                timesheetDetail.setLastUpdatedBy(request.getRemoteUser());
+                timesheetDetail.setLastUpdatedDate(todaysDate);
+                timesheetDate.add(Calendar.DAY_OF_MONTH, 1);
+                java.sql.Date dt = new java.sql.Date(timesheetDate.getTimeInMillis());
+                timesheetDetail.setTimesheetDetailDate(dt);
+                if ( timesheetDate.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY ||
+                    timesheetDate.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ) {
+                    timesheetDetail.setRegularHours(0.0);
+                } else {
+                    timesheetDetail.setRegularHours(8.0);
+                }    
+                
+                // logic goes here for entering some no.
+                timesheetDetail.setOverTimeHours(0.0);                
+                timesheetDetail.setTimesheetDate(sqlDate);                
+                timesheetDetail.setTimesheetUser(request.getRemoteUser());
+                // add to collection
+                timesheetDetailCollection.add(timesheetDetail);
+            }
             
-            TimesheetDetail timesheetDetail = new TimesheetDetail();
-            timesheetDetail.setComments("");
-            timesheetDetail.setCreatedBy(request.getRemoteUser());
-            timesheetDetail.setCreatedDate(todaysDate);
-            timesheetDetail.setDay("");
-            timesheetDetail.setEnabled(true);
-            timesheetDetail.setLastUpdatedBy(request.getRemoteUser());
-            timesheetDetail.setLastUpdatedDate(todaysDate);
-            timesheetDetail.setOverTimeHours(0.0);
-            // logic goes here for entering some no.
-            
-            timesheetDetail.setRegularHours(0.0);
-            timesheetDetail.setTimesheetDate(sqlDate);
-            timesheetDetail.setTimesheetDetailDate(sqlDate);//todo//)
-            timesheetDetail.setTimesheetUser(request.getRemoteUser());
+            timesheet.setTimesheetDetailCollection(timesheetDetailCollection);
+            // get service and save timesheet object
+            TimesheetService timesheetService = (TimesheetService) ServiceFactory.getService("TimesheetService");
+            response.setRenderParameter("success", " Save Successful");
+            try {
+                timesheetService.save(timesheet);
+            } catch (EntityExistsException ex) {
+                response.setRenderParameter("error", " Timesheet Already Exists ");
+                ex.printStackTrace();
+            } catch ( IllegalArgumentException iae) {
+                response.setRenderParameter("error", " Invalid Data");
+                iae.printStackTrace();
+            } catch ( Exception e) {
+                response.setRenderParameter("error", " Error Saving Timesheet");
+                e.printStackTrace();
+            }
         }
-
-
+        
+        
     }
     
     
---
+++ TimesheetSysPortal/src/java/com/abbt/timesheet/services/TimesheetService.java
@@ -0,0 +1,22 @@
+/*
+ * TimesheetService.java
+ *
+ * Created on May 3, 2007, 11:01 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbt.timesheet.services;
+
+import com.abbt.timesheet.entities.Timesheet;
+import com.abbt.timesheet.exceptions.EntityExistsException;
+
+/**
+ *
+ * @author shannan
+ */
+public interface TimesheetService {
+    void save(Timesheet timesheet) throws EntityExistsException;
+    
+}
