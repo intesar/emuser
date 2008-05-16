e60f17fd8b9c35f7f53d626598f4e9e24970ccec
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-05-16 10:53:57 +0000

    updated code with dummy body

---
+++ trunk/BizPayroll/src/java/com/bia/payroll/service/ajax/TimesheetAjaxService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.payroll.service.ajax;
+
+import com.bia.payroll.service.TimesheetService;
+
+/**
+ *
+ * @author intesar
+ */
+public class TimesheetAjaxService {
+
+    public void getAllTimesheets() {
+        
+    }
+    public void getTimesheet () {
+        
+    }
+    public void updateTimesheet ( ) {
+        
+    }
+    
+    public void setTimesheetService(TimesheetService timesheetService) {
+        this.timesheetService = timesheetService;
+    }
+
+    
+    private TimesheetService timesheetService;
+    
+}
