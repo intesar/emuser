0e96ec4ce7b5797a818ceb98a961385b25e64fb1
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-05-16 10:53:57 +0000

    updated code with dummy body

---
+++ BizPayroll/src/java/com/bia/payroll/service/ajax/UserAjaxService.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.payroll.service.ajax;
+
+import com.bia.payroll.service.UserService;
+
+/**
+ *
+ * @author intesar
+ */
+public class UserAjaxService {
+    
+    public void createUser ( ) {
+        
+    }
+    
+    public void getAllUsers() {
+        
+    }
+    
+    public void editUser( )  {
+        
+    }
+    
+    public void changePassword () {
+        
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    
+    private UserService userService;
+    
+}
