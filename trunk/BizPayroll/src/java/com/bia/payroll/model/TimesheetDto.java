185876cf2c9a9c7f0e1a82ca021ef15738a4f01c
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-05-20 11:18:24 +0000

    jars

---
+++ trunk/BizPayroll/src/java/com/bia/payroll/model/TimesheetDto.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.payroll.model;
+
+/**
+ *
+ * @author intesar
+ */
+public class TimesheetDto {
+
+}
--- trunk/BizPayroll/src/java/com/bia/payroll/service/ajax/TimesheetAjaxService.java
+++ trunk/BizPayroll/src/java/com/bia/payroll/service/ajax/TimesheetAjaxService.java
@@ -5,7 +5,12 @@
 
 package com.bia.payroll.service.ajax;
 
+import com.abbhsoft.jpadaoframework.dao.PagedResult;
+import com.bia.payroll.entity.Timesheet;
+import com.bia.payroll.model.TimesheetDto;
 import com.bia.payroll.service.TimesheetService;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,14 +18,29 @@ import com.bia.payroll.service.TimesheetService;
  */
 public class TimesheetAjaxService {
 
-    public void getAllTimesheets() {
+    public List<TimesheetDto> getAllTimesheets(Integer userId, String status) {
+        List<TimesheetDto> timesheetDtos = new ArrayList<TimesheetDto>();
+        PagedResult<Timesheet> pagedResult = timesheetService.getAllTimesheetsByStatus(userId, status);
+        List<Timesheet> timesheets = pagedResult.getResults();
+        for ( Timesheet timesheet : timesheets ) {
+            TimesheetDto dto = new TimesheetDto();
+            // set all required values
+            timesheetDtos.add(dto);
+        }
+        
+        return timesheetDtos;
         
     }
-    public void getTimesheet () {
-        
+    public TimesheetDto getTimesheet (Integer timesheetId) {
+        TimesheetDto dto = new TimesheetDto();
+        Timesheet timesheet = timesheetService.getTimesheet(timesheetId);
+        //copy timesheet into dto
+        return dto;
     }
     public void updateTimesheet ( ) {
-        
+        Timesheet timesheet = new Timesheet();
+        //copy input values into timesheet and merge into db
+        // handle exceptions if successfull send success message else not successful
     }
     
     public void setTimesheetService(TimesheetService timesheetService) {
