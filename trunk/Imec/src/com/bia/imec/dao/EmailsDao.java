d1ba011048a3617cc741ac34d1725a6409f728d6
farazkhan1123 <farazkhan1123@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-05-24 15:26:30 +0000

    created Dto and Daos

---
+++ trunk/Imec/src/com/bia/imec/dao/EmailsDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.imec.dao;
+
+import com.abbhsoft.jpadaoframework.dao.GenericDao;
+import com.bia.imec.entity.Emails;
+
+/**
+ *
+ * @author imran
+ */
+public interface EmailsDao extends GenericDao<Emails , Integer>
+{
+
+}
