e323c5b63673b2d61d997cbe864b4d455462a1cc
farazkhan1123 <farazkhan1123@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-05-24 15:26:30 +0000

    created Dto and Daos

---
+++ Imec/src/com/bia/imec/dao/AuthoritiesDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bia.imec.dao;
+
+import com.abbhsoft.jpadaoframework.dao.GenericDao;
+import com.bia.imec.entity.Authorities;
+import com.bia.imec.entity.AuthoritiesPK;
+
+/**
+ *
+ * @author imran
+ */
+public interface AuthoritiesDao extends GenericDao<Authorities, AuthoritiesPK> {
+}
---
+++ Imec/src/com/bia/imec/dao/MailserversDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.imec.dao;
+
+import com.abbhsoft.jpadaoframework.dao.GenericDao;
+import com.bia.imec.entity.MailServers;
+
+/**
+ *
+ * @author imran
+ */
+public interface MailserversDao extends GenericDao<MailServers , Integer>
+{
+
+}
