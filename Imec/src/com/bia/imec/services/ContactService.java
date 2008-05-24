91160a124dbacc7cf38e207ab764d98aea2f0633
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-05-24 15:27:35 +0000

    updated code

---
+++ Imec/src/com/bia/imec/services/ContactService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.imec.services;
+
+import com.bia.imec.entity.Contacts;
+import java.util.List;
+
+/**
+ *
+ * @author intesar
+ */
+public interface ContactService {
+
+    public List<Contacts> getAll(String Contacts);
+}
