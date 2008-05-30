bea2a6a538d776707abc405a33a06f42942c88e4
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-05-30 13:43:04 +0000

    updated code

--- Imec-Web/src/java/com/bia/imec/service/ajax/util/AcegiUtil.java
+++ Imec-Web/src/java/com/bia/imec/service/ajax/util/AcegiUtil.java
@@ -8,19 +8,6 @@ package com.bia.imec.service.ajax.util;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-import net.sf.acegisecurity.context.security.SecureContext;
-
-import net.sf.acegisecurity.context.security.SecureContextUtils;
-
-
-
-
-
-
-
-
-
-import net.sf.acegisecurity.providers.dao.User;
 
 /**
  *
@@ -32,9 +19,7 @@ public class AcegiUtil {
      *  returns logged in user
      */
     public static final String getUsername() {
-        SecureContext ctx = SecureContextUtils.getSecureContext();
-        User u = (User) ctx.getAuthentication().getPrincipal();
-        return u.getUsername();
+        return null;
     }
 }
 
