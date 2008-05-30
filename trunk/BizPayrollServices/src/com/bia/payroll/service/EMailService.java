b4839e787a39f4a7e0fec166275510441b933fcc
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-05-30 15:47:32 +0000

    updated code

---
+++ trunk/BizPayrollServices/src/com/bia/payroll/service/EMailService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.payroll.service;
+
+
+
+/**
+ *
+ * @author mdshannan
+ */
+public interface EMailService {
+
+    static final String SMTP_HOST_NAME = "smtp.gmail.com";
+    static final String SMTP_PORT = "465";
+    static final String EMAIL_MESSAGE_TEXT = " BizIntelApps ";
+    static final String EMAIL_SUBJECT_TEXT = " Password Request ";
+    static final String EMAIL_FROM_ADDRESS = "mdshannan1@gmail.com";
+    static final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+    static final String[] SEND_TO = {"mohdshannan@yahoo.com"};
+    static final String SEND_FROM_USERNAME = "mdshannan1";
+    static final String SEND_FROM_PASSWORD = "Abbh1234";
+    static final String EMAIL_CONTENT_TYPE = "text/html";
+    void SendMail(String[] sendTo);
+    void sendEmail (String toAddress, String body );
+    void sendEmail (String[] toAddress, String body );
+    
+}
