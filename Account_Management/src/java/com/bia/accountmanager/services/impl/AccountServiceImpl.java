33f670dfe239566b368bda9e51dd6a71bacc5ff2
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-06-24 09:31:58 +0000

    updated code

--- Account_Management/src/java/com/bia/accountmanager/services/impl/AccountServiceImpl.java
+++ Account_Management/src/java/com/bia/accountmanager/services/impl/AccountServiceImpl.java
@@ -2,13 +2,19 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.bia.accountmanager.services.impl;
 
+import com.bia.accountmanager.dao.AccountDao;
+import com.bia.accountmanager.dao.TransactionDao;
+import com.bia.accountmanager.dao.UsersDao;
 import com.bia.accountmanager.entity.Account;
+import com.bia.accountmanager.entity.Transactions;
 import com.bia.accountmanager.services.AccountService;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.transaction.Transaction;
+
 
 /**
  *
@@ -17,27 +23,36 @@ import java.util.List;
 public class AccountServiceImpl implements AccountService {
 
     public void createAccount(Account acct) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.accountDao.create(acct);
     }
 
     public void activateDeactivate(String accountName) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public List<String> getAccountName() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<String> getAccountName(String organization) {
+        List<Account> list = this.accountDao.findByOrganization(organization);
+
+        List<String> l = new ArrayList<String>();
+        for (Account a : list) {
+            l.add(a.getName());
+        }
+
+        return l;
     }
 
     public void duplicate(long TransactionID) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public void deleteTransaction(long transactionid) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void deleteTransaction(Integer transactionid) {
+        Transactions t = transactionDao.read(transactionid);
+        this.transactionDao.delete(t);
     }
 
     public void addTransaction(String account1, String account2, double amount, String memo, Date date) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Transaction s = transactionDao.create(transactionid);
+        this.transactionDao.add(s);
     }
 
     public void withdraw(String account, String payee, double amount, String memo, Date date) {
@@ -52,4 +67,26 @@ public class AccountServiceImpl implements AccountService {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    public void setAccountDao(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    public void setTransactionDao(TransactionDao transactionDao) {
+        this.transactionDao = transactionDao;
+    }
+
+    // setters for dao
+    
+    
+    public void setUsersDao(UsersDao usersDao) {
+        this.usersDao = usersDao;
+    }
+    private AccountDao accountDao;
+    private UsersDao usersDao;
+    private TransactionDao transactionDao;
+    private TransactionDao addtransactionDao;
+
+    public void setAddtransactionDao(TransactionDao addtransactionDao) {
+        this.addtransactionDao = addtransactionDao;
+    }
 }
---
+++ Account_Management/src/java/com/bia/accountmanager/services/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.accountmanager.services.impl;
+
+import com.bia.accountmanager.services.UserServices;
+
+/**
+ *
+ * @author intesar
+ */
+public class UserServiceImpl implements UserServices {
+
+    public void getUsers() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void createUsers(String usersu) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void changePassword(String username, String newpassword) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void deactivateUser(String username) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void activateUser(String username) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void createOrganisation(String organisationname, String usersu) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
