51afdca692c444f6a480708776de49237b0759a2
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-06-24 09:31:58 +0000

    updated code

---
+++ trunk/Account_Management/src/java/com/bia/accountmanager/services/impl/ReportServiceImpl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.accountmanager.services.impl;
+
+import com.bia.accountmanager.services.ReportService;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author intesar
+ */
+public class ReportServiceImpl implements ReportService {
+
+    public List accountSummary(double Account, Date start_date, Date end_date) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
