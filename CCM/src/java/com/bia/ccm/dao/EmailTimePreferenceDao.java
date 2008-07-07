65ca83a239f1867cfd251c28fa8506807e59fc60
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-07-07 13:22:54 +0000

    udpated

--- CCM/src/java/com/bia/ccm/dao/EmailTimePreferenceDao.java
+++ CCM/src/java/com/bia/ccm/dao/EmailTimePreferenceDao.java
@@ -7,12 +7,15 @@ package com.bia.ccm.dao;
 
 import com.abbhsoft.jpadaoframework.dao.GenericDao;
 import com.bia.ccm.entity.EmailTimePreference;
+import java.util.List;
 
 /**
  *
  * @author imran
  */
 public interface EmailTimePreferenceDao extends GenericDao<EmailTimePreference, Integer>{
+
+    public List<EmailTimePreference> findByOrganization(String organization);
     
 
 }
