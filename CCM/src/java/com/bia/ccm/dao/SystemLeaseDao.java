d833329068905a907d990cd84dee7920b0c5a3eb
zaryab.khan14941 <zaryab.khan14941@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-07-07 11:26:54 +0000

    Dao's commited

---
+++ CCM/src/java/com/bia/ccm/dao/SystemLeaseDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.ccm.dao;
+
+import com.bia.ccm.entity.SystemLease;
+import java.util.List;
+
+/**
+ *
+ * @author imran
+ */
+public interface SystemLeaseDao extends GenericDao<SystemLease, String>{
+public List<String>(SystemLease,syslease);
+}
---
+++ CCM/src/java/com/bia/ccm/dao/SystemsPingDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.ccm.dao;
+
+import com.bia.ccm.entity.SystemsPing;
+import java.util.List;
+
+/**
+ *
+ * @author imran
+ */
+public interface SystemsPingDao extends GenericDao<SystemsPing,String>{
+public List<String>(SystemsPing,sysping);
+}
