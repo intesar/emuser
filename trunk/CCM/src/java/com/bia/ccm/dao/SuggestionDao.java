4f76acc3b0be950e1a0a894a067a9646f15ec35a
zaryab.khan14941 <zaryab.khan14941@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-07-07 11:26:54 +0000

    Dao's commited

---
+++ trunk/CCM/src/java/com/bia/ccm/dao/SuggestionDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bia.ccm.dao;
+
+import com.bia.ccm.entity.Suggestion;
+import java.util.List;
+
+/**
+ *
+ * @author imran
+ */
+public interface SuggestionDao extends GenericDao<Suggestion, String>{
+public List<String>(Suggestion,Suggestion);
+}
