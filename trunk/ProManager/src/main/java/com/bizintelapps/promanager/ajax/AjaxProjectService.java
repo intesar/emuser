76f00eee4431d02c3796a631f13d3f46a75b73d7
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2008-11-08 16:44:35 +0000

    skelton

---
+++ trunk/ProManager/src/main/java/com/bizintelapps/promanager/ajax/AjaxProjectService.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2008 intesar.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package com.bizintelapps.promanager.ajax;
+
+/**
+ *
+ * @author intesar
+ */
+public class AjaxProjectService {
+
+    // saveProject should handle create|update
+    // getProjectList should get all projects visible ot user with brief summary
+    // getProjectInfo should get Project with its member list
+    // deleteProject should delete project
+}
---
+++ trunk/ProManager/src/main/java/com/bizintelapps/promanager/entity/Project.java
@@ -0,0 +1,200 @@
+/*
+ *  Copyright 2008 intesar.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package com.bizintelapps.promanager.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author intesar
+ */
+@Entity
+@Table(name = "project")
+@NamedQueries({@NamedQuery(name = "Project.findById", query = "SELECT p FROM Project p WHERE p.id = :id"), @NamedQuery(name = "Project.findByName", query = "SELECT p FROM Project p WHERE p.name = :name"), @NamedQuery(name = "Project.findByDescription", query = "SELECT p FROM Project p WHERE p.description = :description"), @NamedQuery(name = "Project.findByStatus", query = "SELECT p FROM Project p WHERE p.status = :status"), @NamedQuery(name = "Project.findByCreateUser", query = "SELECT p FROM Project p WHERE p.createUser = :createUser"), @NamedQuery(name = "Project.findByCreateDate", query = "SELECT p FROM Project p WHERE p.createDate = :createDate"), @NamedQuery(name = "Project.findByVisibility", query = "SELECT p FROM Project p WHERE p.visibility = :visibility"), @NamedQuery(name = "Project.findByParentProject", query = "SELECT p FROM Project p WHERE p.parentProject = :parentProject")})
+public class Project implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "_status", nullable = false)
+    private String status;
+    @Column(name = "create_user", nullable = false)
+    private String createUser;
+    @Column(name = "create_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+    @Column(name = "visibility", nullable = false)
+    private String visibility;
+    @Column(name = "parent_project")
+    private Integer parentProject;
+    @JoinColumn(name = "organization", referencedColumnName = "id")
+    @ManyToOne
+    private Organization organization;
+    @OneToMany(mappedBy = "project")
+    private Collection<Task> taskCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "project")
+    private Collection<ProjectUsers> projectUsersCollection;
+
+    public Project() {
+    }
+
+    public Project(Integer id) {
+        this.id = id;
+    }
+
+    public Project(Integer id, String name, String status, String createUser, Date createDate, String visibility) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.createUser = createUser;
+        this.createDate = createDate;
+        this.visibility = visibility;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    public Integer getParentProject() {
+        return parentProject;
+    }
+
+    public void setParentProject(Integer parentProject) {
+        this.parentProject = parentProject;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+
+    public Collection<Task> getTaskCollection() {
+        return taskCollection;
+    }
+
+    public void setTaskCollection(Collection<Task> taskCollection) {
+        this.taskCollection = taskCollection;
+    }
+
+    public Collection<ProjectUsers> getProjectUsersCollection() {
+        return projectUsersCollection;
+    }
+
+    public void setProjectUsersCollection(Collection<ProjectUsers> projectUsersCollection) {
+        this.projectUsersCollection = projectUsersCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Project)) {
+            return false;
+        }
+        Project other = (Project) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.bizintelapps.promanager.entity.Project[id=" + id + "]";
+    }
+
+}
