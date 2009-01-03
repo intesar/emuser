cdcdab7ce6ddf4605c8d1adbeef93dd61f0d0da4
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2009-01-03 19:01:13 +0000

    reporting logic

---
+++ trunk/ProManager/src/main/java/com/bizintelapps/promanager/dao/ProjectReportDao.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2009 intesar.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package com.bizintelapps.promanager.dao;
+
+import com.bizintelapps.promanager.entity.ProjectReport;
+
+/**
+ *
+ * @author intesar
+ */
+public interface ProjectReportDao extends GenericDao<ProjectReport, Integer> {
+
+    ProjectReport findByProjectMonthAndYear(Integer project, Integer month, Integer year);
+
+    ProjectReport findByOrganizationMonthAndYear(Integer organization, Integer month, Integer year);
+}
