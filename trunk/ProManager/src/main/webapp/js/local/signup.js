12bf5c18c3166ce582434a0023bcfdb8d5b0cf91
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2009-01-08 02:31:31 +0000

    email logic

--- trunk/ProManager/src/main/webapp/js/local/signup.js
+++ trunk/ProManager/src/main/webapp/js/local/signup.js
@@ -9,13 +9,15 @@ $(document).ready(function () {
         usersDto.username = $('#username').val();
         usersDto.email = usersDto.username;
         usersDto.password = $('#password').val();
+        usersDto.organization = $('#organization').val();     
         var cf = $('#password_confirm').val();
-        if ( usersDto.firstname.length < 2 || usersDto.lastname.length < 2 || usersDto.username.length < 5 ) {
+        if ( usersDto.firstname.length < 2 || usersDto.lastname.length < 2 || usersDto.username.length < 5 ||
+            usersDto.organization.length < 2) {
             alert ( "All field required!");
             return;
         }
         if ( cf != null && cf.length > 4 && cf == usersDto.password) {
-            usersDto.organization = $('#organization').val();                
+                       
             AjaxUsersService.signUp(usersDto, function(result) {
                 alert ( result );
             });   
