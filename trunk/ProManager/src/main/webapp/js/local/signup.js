eefffecb9c2e8f0c1e47c863cea6fd5255967d0e
mdshannan <mdshannan@1dea1c45-a02c-0410-9394-a5799b269e0d>
2009-01-22 02:47:53 +0000

    code udpate for validation

--- trunk/ProManager/src/main/webapp/js/local/signup.js
+++ trunk/ProManager/src/main/webapp/js/local/signup.js
@@ -1,27 +1,60 @@
 
 $(document).ready(function () {
+
+    $('#personal').click ( function() {
+        if ($(this).is(":checked")) {
+            $('#organization').attr('disabled', 'disabled');
+        } else {
+            $('#organization').removeAttr('disabled')
+        }
+    });
+
+    $('#resetForm').click (function() {
+        $('#firstname').val("");
+        $('#lastname').val("");
+        $('#username').val("");
+        $('#password').val("");
+        $('#password_confirm').val("");
+        $('#organization').val("");
+    });
+
     $('#signup').click(function () {
-        var usersDto = {id:null, firstname:null, lastname:null, username:null, password:null, 
-            email:null, organization:null, city:null, country:null};
+        var usersDto = {
+            id:null,
+            firstname:null,
+            lastname:null,
+            username:null,
+            password:null,
+            email:null, 
+            organization:null,
+            city:null,
+            country:null
+        };
             
-        usersDto.firstname=$('#firstname').val();
-        usersDto.lastname = $('#lastname').val();
-        usersDto.username = $('#username').val();
+        usersDto.firstname= $.trim ( $('#firstname').val() );
+        usersDto.lastname = $.trim ( $('#lastname').val() );
+        usersDto.username = $.trim ( $('#username').val() );
         usersDto.email = usersDto.username;
         usersDto.password = $('#password').val();
-        usersDto.organization = $('#organization').val();     
+        usersDto.organization = $.trim ( $('#organization').val() );
+        if ( $('#personal').is(':checked') || $('#organization').val().length < 1 ){
+            usersDto.organization = $('#username').val();
+        } else {
+            usersDto.organization = $('#organization').val();     
+        }
         var cf = $('#password_confirm').val();
-        if ( usersDto.firstname.length < 2 || usersDto.lastname.length < 2 || usersDto.username.length < 5 ||
-            usersDto.organization.length < 2) {
+        if ( usersDto.firstname.length < 2 || usersDto.lastname.length < 2 || usersDto.username.length < 5 ) {
             alert ( "All field required!");
             return;
         }
-        if ( cf != null && cf.length > 4 && cf == usersDto.password) {
-                       
+        if ( cf != null && cf.length > 4 && cf == usersDto.password) {                       
             AjaxUsersService.signUp(usersDto, function(result) {
                 alert ( result );
+                location.href="../view/login.jsp";
             });   
-        } else { alert ("password & confirm password do not match!");}        
+        } else { 
+            alert ("password & confirm password should be same & should have atleast 5 chars!");
+        }
     });
 
 });
